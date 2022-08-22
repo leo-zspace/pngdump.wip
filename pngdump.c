40b932959673a8ca290ecc582d801e3bf180b167
leo <leo@zspace.com>
2022-08-22 14:24:54 -0700

    simplified parameter parser
    also avoided early "returns"
    
    Early "returns", "continue" or "break" inside loops are akin to "goto".
    
    They are hard to follow and they lead to a lot of
    mistakes and unexpected data states.

--- pngdump.c
+++ pngdump.c
@@ -18,141 +18,6 @@
 
 typedef uint8_t byte;
 
-typedef struct option_entry_s {
-    char const* option;
-    char const* format;
-    int count;
-} option_entry_t;
-
-typedef struct command_entry {
-    char const* command;
-    void (*roi)(const byte* data, int x, int y, int w, int h, int stride);
-} command_entry_t;
-
-static void dump(const byte* data, int roiX, int roiY, int roiW, int roiH, int w);
-static void histogram(const byte* data, int roiX, int roiY, int roiW, int roiH, int w);
-static int find_option(const char* option);
-static int find_command(const char* command);
-
-const option_entry_t OPTION_TABLE[] = {
-    {"roi", "%d,%d:%dx%d", 4},
-    {null,  null,          0}
-};
-
-const command_entry_t COMMAND_TABLE[] = {
-    {"dump",      dump     },
-    {"histogram", histogram},
-    {null,        null     }
-};
-
-const char opt_delim[3] = "--\0"; // TODO: "\0" does not do what it seems to do...
-
-int main(int argc, const char* argv[]) {
-    // TODO: the state machine with 2 states is actually 1 "if" statement.
-    //       The command parser is overcomplicated.
-    //       The local variables meaning should be obvious from call site
-    //       and do not deserve the comments.
-    int r     =  0;  /* Return code. */
-    int w     =  0;  /* Image width in pixels.*/
-    int h     =  0;  /* Image height in pixels. */
-    int c     =  0;  /* Number of image components in file. */
-    int i     =  0;  /* Iterator. */
-    int roi_x =  0;  /* ROI x-coordinate. */
-    int roi_y =  0;  /* ROI y-coordinate. */
-    int roi_w =  0;  /* ROI width. */
-    int roi_h =  0;  /* ROI height. */
-    int index = -1;  /* Index of found option/command. */
-    char* token = null;  /* Pointer to string token. */
-    byte* data  = null;  /* Pointer to loaded image data. */
-    enum {
-        PS_OPTIONS = 0,
-        PS_ARGUMENTS= 1
-    };
-    int state = PS_OPTIONS;
-    if (argc > 1) {
-        data = stbi_load("camera.png", &w, &h, &c, 0);
-        if (data == null) {
-            r = errno;
-            perror("file not found\n");
-        } else {
-            for (i = 1; i < argc; i++) {
-                /* Parser state machine. */
-                switch (state) {
-                    case PS_OPTIONS: {
-                        if (strncmp(argv[i], opt_delim, strlen(opt_delim)) == 0) {
-                            // TODO: resolve following issues:
-                            // next line is dangerous - it assumes that argv[i] is modifieable
-                            // which is not true on all platforms. strtok also uses globals
-                            // which is not a great practice to begin with... There is a
-                            // a cleaner way then (char*)argv[i] cast. FIXME
-                            /* Current command line argument is an option. */
-                            token = strtok((char*)argv[i], opt_delim);
-                            if (token != null) {
-                                /* Current command line argument is an option.*/
-                                index = find_option(token);
-                                if (index != -1) {
-                                    /* Located option. Check if option requires additional arguments. */
-                                    if (OPTION_TABLE[index].format != null) {
-                                        /* Option requires additional arguments to be parsed. */
-                                        state = PS_ARGUMENTS;
-                                    }
-                                } else {
-                                    /* Option not found. */
-                                    fprintf(stderr, "%s option not supported.\n", token);
-                                    return EXIT_FAILURE;
-                                }
-                            }
-                        } else {
-                            index = find_command(argv[i]);
-                            if (index != -1) {
-                                /* Execute command.*/
-                                COMMAND_TABLE[index].roi(data, roi_x, roi_y, roi_w, roi_h, w);
-                            } else {
-                                /* Command not found. */
-                                fprintf(stderr, "%s command not supported.\n", argv[i]);
-                                return EXIT_FAILURE;
-                            }
-                        }
-                        break;
-                    }
-                    case PS_ARGUMENTS: {
-                        int retval = sscanf(argv[i], OPTION_TABLE[index].format, &roi_x, &roi_y, &roi_w, &roi_h);
-                        if (retval != OPTION_TABLE[index].count) {
-                            fprintf(stderr, "incorrect number of arguments.");
-                            return EXIT_FAILURE;
-                        } else {
-                            if (roi_x > w || roi_x < 0) {
-                                fprintf(stderr, "x value out of bounds.\n");
-                                return EXIT_FAILURE;
-                            }
-                            if (roi_y > h || roi_y < 0) {
-                                fprintf(stderr, "y out of bounds.\n");
-                                return EXIT_FAILURE;
-                            }
-                            if (roi_x + roi_w > w || roi_w <= 0) {
-                                fprintf(stderr, "W out of bounds.\n");
-                                return EXIT_FAILURE;
-                            }
-                            if (roi_y + roi_h > h || roi_h <= 0) {
-                                fprintf(stderr, "H out of bounds.\n");
-                                return EXIT_FAILURE;
-                            }
-                        }
-                        state = PS_OPTIONS;
-                        break;
-                    }
-                    default: {
-                        fprintf(stderr, "INVALID STATE\n");
-                        break;
-                    }
-                }
-            }
-            free(data);
-        }
-    }
-    return r;
-}
-
 static void dump(const byte* data, int x, int y, int w, int h, int stride) {
     printf("(%d,%d) %dx%d\n", x, y, w, h);
     for (int i = y; i < y + h; i++) {
@@ -177,39 +42,99 @@ static void histogram(const byte* data, int x, int y, int w, int h, int stride) 
     }
 }
 
-// functions below are local to the file and should not
-// expect null as an argument.
-
-static int find_option(const char* option) {
-    assert(option != null); 
-    int index = 0;
-    if (option != null) {
-        while (OPTION_TABLE[index].option != null) {
-            if (strcmp(option, OPTION_TABLE[index].option) == 0) {
-                break;
-            } else {
-                index++;
-            }
-        }
-    } else {
-        index = -1; // this should be an assert()
+static int args_option_index(int argc, const char* argv[], const char* option) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--") == 0) { break; } // no options after '--'
+        if (strcmp(argv[i], option) == 0) { return i; }
     }
-    return index;
+    return -1;
 }
 
-static int find_command(const char* command) {
-    assert(command != null); 
-    int index = 0;
-    if (command != null) {
-        while (COMMAND_TABLE[index].command != null) {
-            if (strcmp(command, COMMAND_TABLE[index].command) == 0) {
-                break;
+static int args_remove_at(int ix, int argc, const char* argv[]) { // returns new argc
+    assert(0 < argc);
+    assert(0 < ix && ix < argc); // cannot remove argv[0]
+    for (int i = ix; i < argc; i++) { argv[i] = argv[i+1]; }
+    argv[argc - 1] = "";
+    return argc - 1;
+}
+
+static int parse_roi(int *argc, const char* argv[], 
+       int *rx, int *ry, int *rw, int *rh, int iw, int ih) {
+    int r = 0;
+    int ix = args_option_index(*argc, argv, "--roi");
+    if (ix >= 0) {
+        if (*argc < ix + 1) {
+            perror("expected --roi X,Y:WxH");
+            r = EXIT_FAILURE;
+        } else { // function does not touch *rx, *ry, *rw, *rh on failure
+            int x = 0;
+            int y = 0;
+            int w = 0;
+            int h = 0;
+            if (sscanf(argv[ix + 1], "%d,%d:%dx%d", &x, &y, &w, &h) != 4) {
+                perror("expected --roi X,Y:WxH");
+                r = EXIT_FAILURE;
+            } else if (0 <= x && x + w <= iw && 0 <= y && y + h <= ih) {
+                *argc = args_remove_at(ix, *argc, argv); // removes "--roi"
+                *argc = args_remove_at(ix, *argc, argv); // removes "X,Y:WxH"
+                *rx = x;
+                *ry = y;
+                *rw = w;
+                *rh = h;
             } else {
-                index++;
+                fprintf(stderr, "%d,%d:%d:%d out of [%d][%d] range\n", 
+                    x, y, w, h, iw, ih);
+                r = EXIT_FAILURE;
             }
         }
-    } else {
-        index = -1; // this should be an assert()
     }
-    return index;
+    return r;
+}
+
+static int usage() {
+    fprintf(stderr, "pngdump [--roi X,Y:WxH] dump|histogram\n");
+    return EXIT_FAILURE;
+}
+
+int main(int argc, const char* argv[]) {
+    int r = 0;
+    int w = 0;
+    int h = 0;
+    int c = 0;
+    byte* data  = null;  /* Pointer to loaded image data. */
+    data = stbi_load("camera.png", &w, &h, &c, 0);
+    if (data == null) {
+        fprintf(stderr, "failed to read \"camera.png\" errno=%d \"%s\"", 
+            errno, strerror(errno));
+        r = EXIT_FAILURE;
+    }
+    if (r == 0) {
+        if (c != 1) {
+            fprintf(stderr, "expected 1 byte per pixel instead of %d"
+                " in file \"camera.png\" %dx%d", c, w, h);
+            r = EXIT_FAILURE;
+        }
+    }
+    int rx = 0; // default roi 0,0:w:h
+    int ry = 0;
+    int rw = w;
+    int rh = h;
+    if (r == 0) {
+        r = parse_roi(&argc, argv, &rx, &ry, &rw, &rh, w, h);
+    }
+    if (r == 0) {
+        if (argc < 1) {
+            perror("expected command: dump or histogram");
+            usage();
+            r = EXIT_FAILURE;
+        } else if (strcmp(argv[1], "dump") == 0) {
+            dump(data, rx, ry, rw, rh, w);
+        } else if (strcmp(argv[1], "histogram") == 0) {
+            histogram(data, rx, ry, rw, rh, w);
+        } else {
+            fprintf(stderr, "unexpected command: %s", argv[1]);
+        }
+    }
+    if (data != null) { free(data); }
+    return r;
 }
